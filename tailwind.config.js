f58e52fc06a120e0035d76236fe3c7d1d98a5f19
Bryan <bryanlampert@gmail.com>
2020-05-15 10:42:59 -0400

    Update tailwind config theme with colors and container centering

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
+  theme: {
+    container: {
+      center: true
+    },
+    colors: {
+      rivalryDark: '#0E142E',
+      rivalryMedium: '#06283B',
+      rivalryLight: '#15435B',
+      rivalryTeal: '#01C8D0',
+      rivalryYellow: '#F6BE23',
+      rivalryOrangeRed: '#FF5A00',
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      red: '#FF4647',
+      redMedium: '#FF9394',
+      redLight: '#FFDFE0'
+    }
+  },
   variants: {},
   plugins: []
 }
